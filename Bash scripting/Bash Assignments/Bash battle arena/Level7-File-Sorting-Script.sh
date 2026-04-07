5eda651edf57515891fbbb4ecc2fbbf7fe78a4a8
Abdulhamid Mohamed <alscholaar@gmail.com>
2026-04-07 00:08:58 -0400

    Update Level7-File-Sorting-Script.sh

--- Bash scripting/Bash Assignments/Bash battle arena/Level7-File-Sorting-Script.sh
+++ Bash scripting/Bash Assignments/Bash battle arena/Level7-File-Sorting-Script.sh
@@ -3,11 +3,15 @@
 # Mission: Write a script that sorts all .txt files in a directory by their size, from smallest to largest, and displays the sorted list.
 
 #!/bin/bash
+if [[ -z "$1" ]]; then
+    echo "directory hasn't been provided"
+    exit 1
+fi
 
 if [[ ! -d "$1" ]]; then
     echo "directory does not exist"
     exit 1
 fi
 
-sort_files=$(ls -l "$1"/*.txt | sort -n)
+sort_files=$(ls -lS "$1"/*.txt 2>/dev/null | tail -r)
 echo "$sort_files"
