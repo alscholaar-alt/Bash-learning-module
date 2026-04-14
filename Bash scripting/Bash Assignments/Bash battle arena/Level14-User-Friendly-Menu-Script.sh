6b6b9eec76ff24b520de3bb18a734891ac6be422
Abdulhamid Mohamed <alscholaar@gmail.com>
2026-04-14 10:21:28 -0400

    Create Level14-User-Friendly-Menu-Script.sh

---
+++ Bash scripting/Bash Assignments/Bash battle arena/Level14-User-Friendly-Menu-Script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Level 14: User-Friendly Menu Script
+# Mission: Create an interactive script that presents a menu with options for different system tasks (e.g., check disk space, show system uptime, list users), and executes the chosen task.
