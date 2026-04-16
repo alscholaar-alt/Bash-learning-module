1e9664be0bfdd0f5ef44cf33f422a5f25a8823e4
Abdulhamid Mohamed <alscholaar@gmail.com>
2026-04-15 21:11:29 -0400

    Add user-friendly menu for system tasks
    Implemented an interactive menu script for system tasks.

--- Bash scripting/Bash Assignments/Bash battle arena/Level14-User-Friendly-Menu-Script.sh
+++ Bash scripting/Bash Assignments/Bash battle arena/Level14-User-Friendly-Menu-Script.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
 # Level 14: User-Friendly Menu Script
+
 # Mission: Create an interactive script that presents a menu with options for different system tasks (e.g., check disk space, show system uptime, list users), and executes the chosen task.
+
+
+echo "Choose an option:"
+echo "1. Check disk space"
+echo "2. Show system uptime"
+echo "3. List users"
+
+read -rp "input your choice numbers 1-3: " choice
+
+case "$choice" in
+    1)
+        echo "The disk space is listed below"
+        df -h
+        ;;
+    2)
+        echo "The system uptime is listed below"
+        uptime
+        ;;
+    3)
+        echo "The users are listed below"
+        cut -d: -f1 /etc/passwd
+        ;;
+    *)
+        echo "Invalid input"
+        ;;
+esac
